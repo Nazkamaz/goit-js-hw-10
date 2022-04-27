b875437cb6820e3abb1eb6d0795caea4c6b02783
Nazariy Bakush <nazkamaz@gmail.com>
2022-04-27 22:11:45 +0300

    hw10

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,35 @@
-// const feald
+import Notiflix, { Notify } from "notiflix";
 
-function fetchCountries(name){
 
+export default function fetchCountries(name){
+    // console.log(name);
+   return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    .then(response => {
+        return response.json();
+    })
+    // .then(countries => {
+    //     console.log(countries);
+    //     if(countries.length > 10){
+    //       return  Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+    //     }
+//         else if(countries.length > 1 && countries.length < 10){
+// const listMarcup = countries.map(({name, flags}) => {
+//     return `<li>
+//     <img
+//             src="${flags.svg}"
+//             alt="${name} flag"
+//             width='14px'
+//     />
+//     <p>${name}</p>
+//     </li>`
+//         })
+// .join('');
+// countriList.insertAdjacentHTML('afterbegin', listMarcup);
+//         }
+//     })
+   
+// .catch(error => {
+//     console.log(error)
+    
+// })
 }
